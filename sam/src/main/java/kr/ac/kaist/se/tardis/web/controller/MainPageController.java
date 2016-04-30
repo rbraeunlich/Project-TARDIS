f5440dfd47d99fa8254ca91534093ab9a56c2720
Ronny Bräunlich <r.braeunlich@gmail.com>
2016-04-30 10:11:06 +0900

    changed the package structure slightly, renamed SAM module to web and added project module

--- sam/src/main/java/kr/ac/kaist/se/tardis/web/controller/MainPageController.java
+++ sam/src/main/java/kr/ac/kaist/se/tardis/web/controller/MainPageController.java
@@ -1,5 +1,8 @@
-package kr.ac.kaist.se.tardis.controller;
+package kr.ac.kaist.se.tardis.web.controller;
 
+import kr.ac.kaist.se.tardis.project.api.ProjectService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +11,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainPageController {
+	
+	@Autowired
+	private ProjectService projectService;
 
 	@RequestMapping(value = { "/index" }, method = RequestMethod.GET)
 	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
 			Model model) {
-		model.addAttribute("name", name);
+		model.addAttribute("name", projectService.foo());
 		return "index";
 	}
 
