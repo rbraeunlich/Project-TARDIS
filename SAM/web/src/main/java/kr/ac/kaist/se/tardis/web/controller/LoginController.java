80604817ab50d9d44861c1d64fa555f366cf5fac
Ronny Bräunlich <r.braeunlich@gmail.com>
2016-05-04 18:11:23 +0900

    create very simple login

---
+++ SAM/web/src/main/java/kr/ac/kaist/se/tardis/web/controller/LoginController.java
@@ -0,0 +1,37 @@
+package kr.ac.kaist.se.tardis.web.controller;
+
+import kr.ac.kaist.se.tardis.project.api.ProjectService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+	
+	@Autowired
+	private ProjectService projectService;
+
+	@RequestMapping(value = { "/index" }, method = RequestMethod.GET)
+	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", projectService.foo());
+		return "index";
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String initialWebpage(Model model) {
+		return "redirect:index";
+	}
+
+	@RequestMapping(value = { "/index" }, method = RequestMethod.POST)
+	public String login(@RequestParam(value = "username", required = true) String username,
+			@RequestParam(value = "password", required = true) String password,
+			Model model) {
+		return "redirect:overview";
+	}
+
+}
--- SAM/web/src/main/java/kr/ac/kaist/se/tardis/web/controller/OverviewController.java
+++ SAM/web/src/main/java/kr/ac/kaist/se/tardis/web/controller/OverviewController.java
@@ -10,20 +10,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class MainPageController {
+public class OverviewController {
 	
 	@Autowired
 	private ProjectService projectService;
 
-	@RequestMapping(value = { "/index" }, method = RequestMethod.GET)
-	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
-			Model model) {
-		model.addAttribute("name", projectService.foo());
-		return "index";
-	}
-
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String initialWebpage(Model model) {
-		return "redirect:index";
+	@RequestMapping(value = { "/overview" })
+	public String greeting( Model model) {
+		return "overview";
 	}
 }
