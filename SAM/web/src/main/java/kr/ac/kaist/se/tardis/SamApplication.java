83b35d6e5dbd5fa75a852c098c3d3636c34569a4
Ronny Bräunlich <r.braeunlich@gmail.com>
2016-05-05 13:31:31 +0900

    -made main page a little bit more pretty

--- SAM/web/src/main/java/kr/ac/kaist/se/tardis/SamApplication.java
+++ SAM/web/src/main/java/kr/ac/kaist/se/tardis/SamApplication.java
@@ -2,10 +2,8 @@ package kr.ac.kaist.se.tardis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class SamApplication {
 
 	public static void main(String[] args) {
--- SAM/web/src/main/java/kr/ac/kaist/se/tardis/web/controller/LoginController.java
+++ SAM/web/src/main/java/kr/ac/kaist/se/tardis/web/controller/LoginController.java
@@ -1,24 +1,16 @@
 package kr.ac.kaist.se.tardis.web.controller;
 
-import kr.ac.kaist.se.tardis.project.api.ProjectService;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-	
-	@Autowired
-	private ProjectService projectService;
 
 	@RequestMapping(value = { "/index" }, method = RequestMethod.GET)
-	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
-			Model model) {
-//		model.addAttribute("name", projectService.foo());
+	public String greeting(Model model) {
 		return "index";
 	}
 
@@ -29,8 +21,7 @@ public class LoginController {
 
 	@RequestMapping(value = { "/index" }, method = RequestMethod.POST)
 	public String login(@RequestParam(value = "username", required = true) String username,
-			@RequestParam(value = "password", required = true) String password,
-			Model model) {
+			@RequestParam(value = "password", required = true) String password, Model model) {
 		return "redirect:overview";
 	}
 
