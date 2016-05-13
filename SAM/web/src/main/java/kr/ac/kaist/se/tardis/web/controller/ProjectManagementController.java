dd8a77fae0a2462b8cfecd74c9e10e2e390e1cbe
Blest215 <blest215@kaist.ac.kr>
2016-05-13 17:11:52 +0900

    Added page of project management
    I have no hash since it's closed issue

---
+++ SAM/web/src/main/java/kr/ac/kaist/se/tardis/web/controller/ProjectManagementController.java
@@ -0,0 +1,27 @@
+package kr.ac.kaist.se.tardis.web.controller;
+
+import javax.validation.Valid;
+
+import kr.ac.kaist.se.tardis.project.api.Project;
+import kr.ac.kaist.se.tardis.project.api.ProjectService;
+import kr.ac.kaist.se.tardis.web.form.CreateProjectForm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ProjectManagementController {
+
+		@Autowired
+		private ProjectService projectService;
+
+		@RequestMapping(value = { "/projectManagement" }, method = RequestMethod.GET)
+		public String projectManagementpage(Model model) {
+			return "projectManagement";
+		}
+	
+}
