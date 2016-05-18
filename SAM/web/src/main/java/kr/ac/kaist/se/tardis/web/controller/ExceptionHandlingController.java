dbb86df599317e3a24dab68948b2311d92644f58
Ronny Bräunlich <r.braeunlich@gmail.com>
2016-05-18 12:12:10 +0900

    created an error page

---
+++ SAM/web/src/main/java/kr/ac/kaist/se/tardis/web/controller/ExceptionHandlingController.java
@@ -0,0 +1,27 @@
+package kr.ac.kaist.se.tardis.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.http.HttpStatus;
+
+@Controller
+public class ExceptionHandlingController implements ErrorController {
+
+	@ResponseStatus(code=HttpStatus.NOT_FOUND)
+	public String notFound(){
+		return "errorpage";
+	}
+	
+	@ResponseStatus(code=HttpStatus.FORBIDDEN)
+	@RequestMapping("error")
+	public String forbidden(){
+		return "errorpage";
+	}
+
+	@Override
+	public String getErrorPath() {
+		return "/error";
+	}
+}
