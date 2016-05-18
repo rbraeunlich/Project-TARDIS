6424e2ccd3f63b4784d727922d982393c9e4f2fe
Ronny Bräunlich <r.braeunlich@gmail.com>
2016-05-18 18:50:15 +0900

    extended the security model
    -add HTTPS to the website
    -split the configuration into two modes, default and dev
    -dev mode does not use any authentication
    -because there is only a null user, every call to getUsername() had to
    be wrapped in String.valueOf()
    -removed admin/admin user

--- SAM/web/src/main/java/kr/ac/kaist/se/tardis/web/controller/KanbanBoardController.java
+++ SAM/web/src/main/java/kr/ac/kaist/se/tardis/web/controller/KanbanBoardController.java
@@ -1,21 +1,14 @@
 package kr.ac.kaist.se.tardis.web.controller;
 
-import javax.validation.Valid;
-
-import kr.ac.kaist.se.tardis.project.api.Project;
-import kr.ac.kaist.se.tardis.project.api.ProjectService;
-import kr.ac.kaist.se.tardis.web.form.CreateProjectForm;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import kr.ac.kaist.se.tardis.project.api.ProjectService;
 
 @Controller
 public class KanbanBoardController {
@@ -25,7 +18,7 @@ public class KanbanBoardController {
 		
 		@RequestMapping(value = { "/kanbanboard" }, method = RequestMethod.GET)
 		public String projectManagementpage(Model model, @AuthenticationPrincipal UserDetails user) {
-			model.addAttribute("username", user.getUsername());
+			model.addAttribute("username", String.valueOf(user.getUsername()));
 			return "KanbanBoard";
 		}
 	
--- SAM/web/src/main/java/kr/ac/kaist/se/tardis/web/controller/LoginController.java
+++ SAM/web/src/main/java/kr/ac/kaist/se/tardis/web/controller/LoginController.java
@@ -1,5 +1,9 @@
 package kr.ac.kaist.se.tardis.web.controller;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +20,14 @@ public class LoginController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String initialWebpage(Model model) {
 		return "redirect:index";
+	}
+
+	@Profile("dev")
+	@RequestMapping(value = { "/index" }, method = RequestMethod.POST)
+	public String login() {
+		Authentication auth = new TestingAuthenticationToken("Test-User", "");
+		SecurityContextHolder.getContext().setAuthentication(auth);
+		return "redirect:overview";
 	}
 
 }
