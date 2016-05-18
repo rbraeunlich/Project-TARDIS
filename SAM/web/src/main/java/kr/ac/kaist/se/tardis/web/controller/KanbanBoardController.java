96f796a7728125530f27aff7812acd1ac1ba9a85
Ronny Bräunlich <r.braeunlich@gmail.com>
2016-05-18 19:09:49 +0900

    resolved conflict

--- SAM/web/src/main/java/kr/ac/kaist/se/tardis/web/controller/KanbanBoardController.java
+++ SAM/web/src/main/java/kr/ac/kaist/se/tardis/web/controller/KanbanBoardController.java
@@ -1,15 +1,5 @@
 package kr.ac.kaist.se.tardis.web.controller;
 
-<<<<<<< HEAD
-import javax.validation.Valid;
-
-import kr.ac.kaist.se.tardis.project.api.Project;
-import kr.ac.kaist.se.tardis.project.impl.id.*;
-import kr.ac.kaist.se.tardis.project.api.ProjectService;
-import kr.ac.kaist.se.tardis.web.form.CreateProjectForm;
-
-=======
->>>>>>> origin/master
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,8 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.ac.kaist.se.tardis.project.api.ProjectService;
+import kr.ac.kaist.se.tardis.project.impl.id.ProjectId;
+import kr.ac.kaist.se.tardis.project.impl.id.ProjectIdFactory;
 
 @Controller
 public class KanbanBoardController {
@@ -27,16 +20,11 @@ public class KanbanBoardController {
 		private ProjectService projectService;
 		
 		@RequestMapping(value = { "/kanbanboard" }, method = RequestMethod.GET)
-<<<<<<< HEAD
 		public String projectManagementpage(Model model, @RequestParam(name = "projectId", required = true) String projectId, @AuthenticationPrincipal UserDetails user) {
-			model.addAttribute("username", user.getUsername());
+			model.addAttribute("username", String.valueOf(user.getUsername()));
 			
 			ProjectId id = ProjectIdFactory.valueOf(projectId);
 			model.addAttribute("project", projectService.findProjectById(id).get());
-=======
-		public String projectManagementpage(Model model, @AuthenticationPrincipal UserDetails user) {
-			model.addAttribute("username", String.valueOf(user.getUsername()));
->>>>>>> origin/master
 			return "KanbanBoard";
 		}
 	
