e396ac30d5ba30b92394cdec7d7fd564f041897d
Ronny Bräunlich <r.braeunlich@gmail.com>
2016-05-23 20:04:38 +0900

    removed no owner error, since the check for existing project member is enough

--- SAM/web/src/main/java/kr/ac/kaist/se/tardis/web/form/SetTaskForm.java
+++ SAM/web/src/main/java/kr/ac/kaist/se/tardis/web/form/SetTaskForm.java
@@ -9,7 +9,6 @@ public class SetTaskForm {
 	public static final String NO_TASK_NAME_ERROR = "Task must have a name";
 	public static final String DUP_TASK_NAME_ERROR = "Task must have a unique name";
 	public static final String NO_DUE_DATE_ERROR = "Task must set due date";
-	public static final String NO_OWNER_ERROR = "Task must assigned to owner";
 	public static final String OWNER_NOT_EXIST_ERROR = "Owner is not registered user";
 	public static final String NO_EXISITING_MEMBER = "No Existing Member";
 	public static final String WRONG_DUE_DATE_ERROR = "yyyy-MM-dd";
@@ -28,7 +27,6 @@ public class SetTaskForm {
 	private String dueDate;
 
 	@NotNull
-	@Size(min = 1, message = NO_OWNER_ERROR)
 	private String owner;
 
 	public String getTaskName() {
